37860fe49d6ce99de922c9600aead96d0aeac3aa
Geetansh Rahangdale <geetansh335@gmail.com>
2023-02-08 01:38:07 +0530

    Setup 0002

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/';
+import App from './App';
+
+const root = ReactDOM.createroot(document.getElementById('root'));
+root.render(
+    <App />
+);
